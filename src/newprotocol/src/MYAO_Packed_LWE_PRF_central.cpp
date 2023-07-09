336d789ee0f44cdefe3138a7d74460da92543e62
jonny9066 <jon@Jon-ThinkPad>
2023-07-09 13:27:11 +0300

    converting converting input to Z3 and matrix multiplication Z2 done

--- src/newprotocol/src/MYAO_Packed_LWE_PRF_central.cpp
+++ src/newprotocol/src/MYAO_Packed_LWE_PRF_central.cpp
@@ -55,39 +55,58 @@ void PRF_packed_test(std::vector<uint64_t>& K1, PackedZ2<N_COLS>& x1, std::vecto
     {
         K[i] = K1[i] ^ K2[i];
     }
-    PackedZ2<N_COLS> outKX; //stores the output of K*x in Z2
 
-    // TODO convert from z2 to z3
+    // convert input and K from z2 to z3
+    PackedZ2<N_SIZE> K_Z2;
+    K_Z2.makeFromBits(K); // convert to Packed format
 
-    //TODO multiply x by k in z3
-
-    // TODO convert result to z2
-
-    // TODO xor the results
-
-    //TODO apply final transformation
-
-    auto start_prf = std::chrono::system_clock::now();//start the clock
-
-    auto start_p1 = chrono::system_clock::now();//start the clock
-    //Since K is a toeplitz, this function performs matrix vector multiplication where matrix is of kind toeplitz
-    outKX.toeplitzByVec(K,X);
-    timer_packed_cent_p1 += (std::chrono::system_clock::now() - start_p1).count();//time the clock and take difference from start_p1
-
-    PackedZ3<256> outKX_Z3;//Z2 to Z3 converted output of phase 1
+    PackedZ3<N_COLS> X_Z3;
+    PackedZ3<N_COLS> K_Z3;
 
     PackedZ2<N_SIZE> hi; //MSB will be initialized to 0, since Z2 to Z3 won't affect the MSB's
 
-    auto start_p2 = chrono::system_clock::now();
-    outKX_Z3.makeFromBits(hi.bits, outKX.bits);//outKX_Z3 is a Z3 containing 0/1 values in Z2
-    timer_packed_cent_p2 += (std::chrono::system_clock::now() - start_p2).count();//time the clock and take difference from start_p2
+    K_Z3.makeFromBits(hi.bits, K_Z2.bits);
+    X_Z3.makeFromBits(hi.bits, X.bits);
+
+
+    // perform matrix multiplication in Z2
+    PackedZ2<N_COLS> outKX_Z2; //stores the output of K*x in Z2
+
+    //Since K is a toeplitz, this function performs matrix vector multiplication where matrix is of kind toeplitz
+    outKX_Z2.toeplitzByVec(K,X);
+
+
+    //TODO multiply x by k in z3
+    PackedZ3<N_COLS> outKX_Z3; //stores the output of K*x in Z3
+
+
+
+
+    // TODO convert result to z2
+
+
+    // TODO xor the results
+
+
+    //TODO apply final transformation
+
+    // auto start_prf = std::chrono::system_clock::now();//start the clock
+
+    // auto start_p1 = chrono::system_clock::now();//start the clock
+    // timer_packed_cent_p1 += (std::chrono::system_clock::now() - start_p1).count();//time the clock and take difference from start_p1
+
+
+
+    // auto start_p2 = chrono::system_clock::now();
+    // outKX_Z3.makeFromBits(hi.bits, outKX.bits);//outKX_Z3 is a Z3 containing 0/1 values in Z2
+    // timer_packed_cent_p2 += (std::chrono::system_clock::now() - start_p2).count();//time the clock and take difference from start_p2
 
     //PackedZ3<81> outZ3;//final Z3 output
     auto start_p3 = chrono::system_clock::now();        //start the clock and save the time.
     outZ3.matByVec(Rmat,outKX_Z3);              //output of Rmat * (K * x); output of wPRF
     timer_packed_cent_p3 += (std::chrono::system_clock::now() - start_p3).count();//time the clock and take difference from start_p3
 
-    timer_PRF_packed += (std::chrono::system_clock::now() - start_prf).count();
+    // timer_PRF_packed += (std::chrono::system_clock::now() - start_prf).count();
 
 }
 
@@ -113,21 +132,22 @@ void PRF_packed(int nTimes,  int nRuns, int nStages)
 //Displays the timing after measuring unit metric for the machine it runs in.
 void display_times(int nRuns)
 {
-    using Clock = std::chrono::system_clock;
-    using Duration = Clock::duration;
-    //std::cout << Duration::period::num << " , " << Duration::period::den << '\n';
-    float time_unit_multiplier = 1;
-    if(Duration::period::den == 1000000000)
-        time_unit_multiplier = 0.001; //make nanosecond to microsecond
-    else if(Duration::period::den == 1000000)
-        time_unit_multiplier = 1;   //keep the unit as microsecond
+    std::cout<<"displaying times not implemented yet"<<std::endl;
+    // using Clock = std::chrono::system_clock;
+    // using Duration = Clock::duration;
+    // //std::cout << Duration::period::num << " , " << Duration::period::den << '\n';
+    // float time_unit_multiplier = 1;
+    // if(Duration::period::den == 1000000000)
+    //     time_unit_multiplier = 0.001; //make nanosecond to microsecond
+    // else if(Duration::period::den == 1000000)
+    //     time_unit_multiplier = 1;   //keep the unit as microsecond
 
-    std::cout<<"Time for phase 1(K*X), "<<nRuns << " runs = " << (timer_packed_cent_p1 * time_unit_multiplier)<<" microseconds"<<  std::endl;
-    std::cout<<"Time for phase 2(casting w from Z2 to Z3), "<<nRuns << " runs = " << (timer_packed_cent_p2 * time_unit_multiplier)<<" microseconds"<<  std::endl;
-    std::cout<<"Time for phase 3(Randomization multiplication) "<<nRuns << " runs = " <<(timer_packed_cent_p3 * time_unit_multiplier)<<" microseconds" << std::endl;
-    std::cout<<"Time for entire packed centralized PRF,  "<<nRuns << " runs = " <<(timer_PRF_packed  * time_unit_multiplier)<<" microseconds"<< std::endl;
-    std::cout<<"Number of rounds per second for phase 1(K*X) "<<(1000/(timer_packed_cent_p1*time_unit_multiplier)*1000000)<<std::endl;
-    std::cout<<"Number of rounds per second for phase 2(casting w from Z2 to Z3) "<<(1000/(timer_packed_cent_p2*time_unit_multiplier)*1000000)<<std::endl;
-    std::cout<<"Number of rounds per second for phase 3(Randomization multiplication) "<<(1000/(timer_packed_cent_p3*time_unit_multiplier)*1000000)<<std::endl;
-    std::cout<<"Number of rounds per second for entire packed centralized PRF "<<(1000/(timer_PRF_packed*time_unit_multiplier)*1000000)<<std::endl;
+    // std::cout<<"Time for phase 1(K*X), "<<nRuns << " runs = " << (timer_packed_cent_p1 * time_unit_multiplier)<<" microseconds"<<  std::endl;
+    // std::cout<<"Time for phase 2(casting w from Z2 to Z3), "<<nRuns << " runs = " << (timer_packed_cent_p2 * time_unit_multiplier)<<" microseconds"<<  std::endl;
+    // std::cout<<"Time for phase 3(Randomization multiplication) "<<nRuns << " runs = " <<(timer_packed_cent_p3 * time_unit_multiplier)<<" microseconds" << std::endl;
+    // std::cout<<"Time for entire packed centralized PRF,  "<<nRuns << " runs = " <<(timer_PRF_packed  * time_unit_multiplier)<<" microseconds"<< std::endl;
+    // std::cout<<"Number of rounds per second for phase 1(K*X) "<<(1000/(timer_packed_cent_p1*time_unit_multiplier)*1000000)<<std::endl;
+    // std::cout<<"Number of rounds per second for phase 2(casting w from Z2 to Z3) "<<(1000/(timer_packed_cent_p2*time_unit_multiplier)*1000000)<<std::endl;
+    // std::cout<<"Number of rounds per second for phase 3(Randomization multiplication) "<<(1000/(timer_packed_cent_p3*time_unit_multiplier)*1000000)<<std::endl;
+    // std::cout<<"Number of rounds per second for entire packed centralized PRF "<<(1000/(timer_PRF_packed*time_unit_multiplier)*1000000)<<std::endl;
 }
